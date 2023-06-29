780af4da38be9fc9d2bdbb879eb1043a3379d18d
JackieCisneros <krieger.jacqueline@gmail.com>
2023-06-29 11:02:29 -0600

    updated pic and took out a few unnecessary elements

--- personalproject/src/Home.jsx
+++ personalproject/src/Home.jsx
@@ -111,8 +111,9 @@ export default function Home(){
                     borderRadius: '10px'
                     }}>
                     <div className="column" style={{paddingTop:'8vh', display:'flex', flexDirection:'column', alignItems:'flex-start'}}>
-                        <h1 style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)', fontSize:'5.5vh'}}>Jacqueline Krieger-Cisneros</h1>
-                        <h4 style={{fontSize:'3.8vh'}}>Full-Stack Developer, VISION for AI</h4>
+                        <h1 style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)', fontSize:'5vh'}}>Jacqueline Krieger-Cisneros</h1>
+                        <h4 style={{fontSize:'3.2vh'}}>Full-Stack Developer</h4>
+                        <h5 style={{fontSize:'2.5vh', color:'gray'}}>Anthropologist | Environmental Advocate </h5>
                     </div>
 
                     <div style={{padding:'5vh'}}></div>
@@ -124,37 +125,39 @@ export default function Home(){
                         paddingLeft: "25px",
                         paddingRight: "25px", 
                         borderRadius:'10px', 
+
                     }} className="row">
                         <div style={{display:'flex', flexDirection:'column', alignItems:'flex-end', position:'relative',left:'-5%', paddingTop:'5vh', paddingRight:'15vw'}}>
                             <h2 className='' style={{
-                                fontSize:'4vh',
+                                fontSize:'3.2vh',
                                 color:'black', 
                                 textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)'
                             }}>
-                                Python | JS | SQL
+                                Python | JavaScript | SQL
                             </h2>
                             <h3 className='' style={{paddingTop:'2vh', color:'black', 
-                            textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)', fontSize:'4vh',}}>
+                            textShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)', fontSize:'3vh',}}>
                                 Flask | React | Postgres | Git | AWS
                             </h3>
                         </div>
                         
 
 
-                        <div>
-                        
-                            <img style={{width:'30vw', borderRadius:'50%', position:'absolute', right:'55%', bottom:'-22%'}} src="https://phase-4-bucket-2.s3.us-west-2.amazonaws.com/%E2%80%94Pngtree%E2%80%94abstract+boheiman+eye+mystic+logo_7595916.png"></img>
-                            <div style={{display:"flex", justifyContent: 'flex-end', paddingTop:'50px', position: 'relative'}}>
-                                <img 
-                                    src="https://phase-5-images.s3.us-west-2.amazonaws.com/5780dc1e5bb34a28b89ac7f016f6abdf.png"
-                                    className="overlap-image1 rounded-circle"
-                                    style={{zIndex: 2, width:'16vw', height:'85%',position:'absolute', left:'66%', top:'9%'}}
-                                ></img>
-                                <img 
-                                    src="https://phase-5-images.s3.us-west-2.amazonaws.com/IMG_1538.jpg"
-                                    className="overlap-image2 rounded-circle"
-                                    style={{opacity: '0.5', zIndex: 1, left: '-27%', width: '16vw', position:'relative'}}
-                                ></img>
+                        <div >
+                            {/* <img style={{width:'30vw', borderRadius:'50%', position:'absolute', right:'55%', bottom:'-12%'}} src="https://phase-4-bucket-2.s3.us-west-2.amazonaws.com/%E2%80%94Pngtree%E2%80%94abstract+boheiman+eye+mystic+logo_7595916.png"></img> */}
+                            <div>       
+                                <div style={{display:"flex", justifyContent: 'flex-end', paddingTop:'50px', position: 'relative'}}>
+                                    <img 
+                                        src="https://phase-5-images.s3.us-west-2.amazonaws.com/7259476fb3494fbca15520956c8c75ed.png"
+                                        className="overlap-image1 rounded-circle"
+                                        style={{zIndex: 2, width:'16vw',position:'absolute', left:'23%', top:'-8%'}}
+                                    ></img>
+                                    <img 
+                                        src="https://phase-5-images.s3.us-west-2.amazonaws.com/Jackie's+Portrait.jpeg"
+                                        className="overlap-image2 rounded-circle"
+                                        style={{opacity: '0.5', zIndex: 1, left: '-70%', width: '16vw', position:'relative'}}
+                                    ></img>
+                                </div> 
                             </div>
 
 
@@ -162,17 +165,18 @@ export default function Home(){
                                     <OverlayTrigger trigger="click" placement="right" overlay={popover}>
                                         <button
                                             className="connect"
-                                            style={{width: '4vw',
-                                            height: '8vh',
-                                            backgroundColor: '#F6E6D8',
-                                            boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.25)',
-                                            color: 'black',
-                                            borderRadius: '50%',
-                                            display: 'flex',
-                                            justifyContent: 'center',
-                                            alignItems: 'center',
-                                            border: 'none',}}
-                                        >ℹ️</button>
+                                            style={{
+                                                width: '4vw',
+                                                height: '6vh',
+                                                backgroundColor: '#F6E6D8',
+                                                boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.25)',
+                                                color: 'black',
+                                                borderRadius: '50%',
+                                                display: 'flex',
+                                                justifyContent: 'center',
+                                                alignItems: 'center',
+                                                border: 'none',}}
+                                            >ℹ️</button>
                                     </OverlayTrigger>
                                 </div>
                         </div>
@@ -180,36 +184,38 @@ export default function Home(){
 
                     <div style={{padding:'15vh'}}></div>
 
-                    <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+                    <div style={{display:'flex', justifyContent:'center', alignItems:'center', }}>
                         
-                    <div style={{display:'flex', flexDirection:'column', justifyContent:'center', marginLeft: '-7vw',backgroundColor: '#e4c3b1'}}>
+                    {/* <div style={{display:'flex', flexDirection:'column', justifyContent:'center', marginLeft: '-7vw',backgroundColor: '#e4c3b1'}}>
                         <img src="https://phase-4-bucket-2.s3.us-west-2.amazonaws.com/moonphasecolorratio3-4.jpg" style={{width:'20vw', height:'55vh', objectFit:'contain'}}></img>
                         <img src="https://phase-4-bucket-2.s3.us-west-2.amazonaws.com/moonphasecolorratio3-4.jpg" style={{width:'20vw', height:'55vh', objectFit:'contain'}}></img>
                         <img src="https://phase-4-bucket-2.s3.us-west-2.amazonaws.com/moonphasecolorratio3-4.jpg" style={{width:'20vw', height:'55vh', objectFit:'contain'}}></img>
-                        
                         <br></br>
                      
-                    </div>
+                    </div> */}
+                        {/* opacity:".5", backgroundImage: `url("https://phase-5-images.s3.us-west-2.amazonaws.com/ocean+bubble.jpeg")`, height: '100vh', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' */}
                     
                     <div id='about'>
                         <h1 style={{textAlign: 'right'}}> . . . </h1>
                         <br></br>
-                        <h1 style={{textAlign: 'right', fontSize:'4.5vh', position:'relative', left:'-2vh'}}>Welcome</h1>
+                        <h1 style={{textAlign: 'right', fontSize:'3.5vh', position:'relative', left:'-2vh'}}>Welcome</h1>
                         <br></br>
-                        <p style={{textAlign: 'left', fontSize:'3vh', paddingLeft:'1vw'}}>Thank you for choosing to be here! Although I enjoy spending my time in Python, I have built this site using React.js to help you better understand who I am and how I can contribute to your software needs. Whether you are looking to collaborate on a project, need a developer for a specific task, or are seeking a valuable addition to your team, feel free to reach out! We may be a great professional fit.</p>
+                        <p style={{textAlign: 'right', fontSize:'2.5vh', paddingLeft:'1vw'}}>Thank you for choosing to be here! Python is my language of choice, and I have built this site using React.js to help you better understand who I am and how I can contribute to your software needs. Whether you are looking to collaborate on a project, need a developer for a specific task, or are seeking a valuable addition to your team, feel free to reach out! We may be a great professional fit.</p>
                             <br></br>
-                        <h3 style={{textAlign: 'right',fontSize:'3.7vh', position:'relative', left:'-2vh'}}>About Me</h3>
-                            <p style={{textAlign: 'left', fontSize:'3vh', paddingLeft:'1vw'}}>
+                        <h3 style={{textAlign: 'right',fontSize:'3vh', position:'relative', left:'-2vh'}}>About Me</h3>
+                            <p style={{textAlign: 'right', fontSize:'2.5vh', paddingLeft:'1vw'}}>
                             In August of 2022, I happily stumbled across FreeCodeCamp and began working through their program. I was amazed by the remarkable results that could be achieved with a simple amount of code. This sparked my passion for programming, leading me to study JavaScript and eventually enroll in a SE bootcamp where I spent an intense 15 weeks learning full-stack development. I absolutely love what I do and am excited to bring my diverse background and experiences to the table. Previously, I studied Anthropology and Dance during my college years. I then moved to Hawaii and became a Dive Master, gaining valuable experience while working in high-end resorts. Later, the most fortunate event in my life occurred when I started a family. All of these experiences have shaped me, fostering curiosity, playfulness, attentive focus, and creative problem-solving skills that greatly benefit my profession as a developer.   
                         </p>
-                        <h3 style={{textAlign: 'right', fontSize:'3.7vh', position:'relative', left:'-2vh'}}>Currently Working On</h3>
-                            <p style={{textAlign: 'left', fontSize:'3vh', paddingLeft:'1vw'}}>
+                        <br></br>
+                        <h3 style={{textAlign: 'right', fontSize:'3vh', position:'relative', left:'-2vh'}}>Currently Working On</h3>
+                            <p style={{textAlign: 'right', fontSize:'2.5vh', paddingLeft:'1vw'}}>
                             I am currently focusing on mastering algorithms. My goal is to enhance my code's efficiency, speed, and cost-effectiveness to bring maximum benefits to your projects. Additionally, I have a keen interest in Artificial Intelligence (AI) and how I can leverage this technology, particularly in model building, within my development profession. We find ourselves in an exciting era where AI has become the tool of the decade, and I am eager to contribute to its advancements.
                         </p>
-                        <h3 style={{textAlign: 'right', fontSize:'3.7vh', position:'relative', left:'-2vh'}}>Skills</h3>
-                            <p style={{textAlign: 'left', fontSize:'3vh', paddingLeft:'1vw'}}>
+                        <br></br>
+                        <h3 style={{textAlign: 'right', fontSize:'3vh', position:'relative', left:'-2vh'}}>Skills</h3>
+                            <p style={{textAlign: 'right', fontSize:'2.5vh', paddingLeft:'1vw'}}>
                             With a proven track record of quickly grasping new concepts and implementing research findings effectively, I thrive in fast-paced environments where I can adapt and apply new knowledge almost immediately. 
-                            I was honored with the "Makes Everything Look Easy" Award at my coding bootcamp, recognizing my exceptional ability to simplify complex concepts and make them accessible to others. This skill allows me to communicate effectively and collaborate seamlessly with team members, stakeholders, and clients. Additionally, I have experience working with complex and relevant APIs, such as Google Cloud Translate and Stability AI, skillfully integrating them into applications for seamless data exchange and enhanced functionality. Leveraging my expertise in AWS S3 (Simple Storage Service), I efficiently configure and manage buckets to ensure secure and reliable storage of data and media assets in the cloud. 
+                            I was honored with the "Makes Everything Look Easy" Award at my coding bootcamp, recognizing my exceptional ability to simplify complex concepts and make them accessible to others. Additionally, I have experience working with complex and relevant APIs, such as Google Cloud Translate and Stability AI, skillfully integrating them into applications for seamless data exchange and enhanced functionality. Leveraging my expertise in AWS S3 (Simple Storage Service), I efficiently configure and manage buckets to ensure secure and reliable storage of data and media assets in the cloud. 
                             </p>
                         <br></br>
                         <h1 style={{textAlign: 'right'}}> . . . </h1>
